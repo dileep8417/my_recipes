a1849a29c41c07571ad92d85996cfe4b4248ea1f
unknown <dileep8417@gmail.com>
2023-04-14 06:03:51 +0530

    Initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Recipes from './components/Recipes';
+import Favourites from './components/Favourites';
+import Search from './components/Search';
+import Modal from './components/Modal';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <Navbar />
+      <Search />
+      <Favourites />
+      <Recipes />
+      <Modal />
+      <Footer />
     </div>
   );
 }
